4cf0b033fb13bd1ed8f4a37848b616b4f022b7df
ehsan <ehsan@DESKTOP-JVII58A>
2021-10-05 22:58:27 +0530

    Microservice Foundation

---
+++ laptop-shopping/src/main/java/com/shopping/laptopshopping/LaptopShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.laptopshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopShoppingApplication.class, args);
+	}
+
+}
---
+++ laptop-shopping/src/test/java/com/shopping/laptopshopping/LaptopShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.laptopshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
